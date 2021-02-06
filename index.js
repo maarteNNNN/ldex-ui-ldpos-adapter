72c0a2da4219706c19941246b65f86f745eb5b55
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-02-06 01:42:24 +0100

    Simplify logic and bump dependency

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ const { createClient } = require('ldpos-client');
 const DEFAULT_API_MAX_PAGE_SIZE = 100;
 const DEFAULT_API_TIMEOUT = 20000;
 const DEFAULT_NETHASH = 'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba';
-const DEFAULT_PROTOCOL_VERSION = '1.1';
+const DEFAULT_SOCKET_PROTOCOL_VERSION = 1;
+const DEFAULT_PEER_PROTOCOL_VERSION = '1.1';
 const DEFAULT_CLIENT_VERSION = '2.0.0';
-const DEFAULT_INBOUND_PORT = 0;
 
 class LDPoSAdapter {
   constructor(options) {
     this.apiURL = options.apiURL;
+    this.chainSymbol = options.chainSymbol;
     this.apiMaxPageSize = options.apiMaxPageSize || DEFAULT_API_MAX_PAGE_SIZE;
     this.apiTimeout = options.apiTimeout || DEFAULT_API_TIMEOUT;
     this.nethash = options.nethash || DEFAULT_NETHASH;
-    this.protocolVersion = options.protocolVersion || DEFAULT_PROTOCOL_VERSION;
+    this.socketProtocolVersion = options.socketProtocolVersion || DEFAULT_SOCKET_PROTOCOL_VERSION;
+    this.peerProtocolVersion = options.peerProtocolVersion || DEFAULT_PEER_PROTOCOL_VERSION;
     this.clientVersion = options.clientVersion || DEFAULT_CLIENT_VERSION;
-    this.inboundPort = options.inboundPort || DEFAULT_INBOUND_PORT;
 
     let urlParts = url.parse(this.apiURL);
 
     this.client = createClient({
+      networkSymbol: this.chainSymbol,
       hostname: urlParts.hostname,
       port: parseInt(urlParts.port),
-      protocolVersion: this.protocolVersion,
+      socketProtocolVersion: this.socketProtocolVersion,
+      peerProtocolVersion: this.peerProtocolVersion,
       nethash: this.nethash,
-      inboundPort: this.inboundPort,
       clientVersion: this.clientVersion
     });
   }
@@ -49,8 +51,12 @@ class LDPoSAdapter {
     });
   }
 
-  createWallet({ networkSymbol }) {
-    return this.client.generateWallet({ networkSymbol });
+  createWallet() {
+    return this.client.generateWallet();
+  }
+
+  getAddressFromPassphrase({ passphrase }) {
+    return this.client.computeWalletAddressFromPassphrase(passphrase);
   }
 
   validatePassphrase({ passphrase }) {
